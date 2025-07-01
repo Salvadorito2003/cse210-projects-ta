ee636ca6c1ceb493c03f586da87a6e64feb5d9b8
Tobias <tobiasaguilartarea@gmail.com>
2025-07-01 20:30:51 -0300

    Did week 1 exercise 1 and 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        string userImput = Console.ReadLine();
+        int number = int.Parse(userImput);
+        string grade = "";
+        string sign = "";
+        if (number >= 90)
+        {
+            grade = "A";
+        }
+        else if (number >= 80)
+        {
+            grade = "B";
+        }
+        else if (number >= 70)
+        {
+            grade = "C";
+        }
+        else if (number >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        
+        if (!(grade == "A" || grade == "F") && (number % 10 >= 7))
+        {
+            sign = "+";
+        }
+        else if (!(grade == "F") && (number % 10 < 3))
+        {
+            sign = "-";
+        }
+
+        Console.Write($"Your grade is: {grade}{sign}");
     }
+
 }

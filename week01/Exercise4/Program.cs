3d0b3c905117c23297bffebcdc5cf77696fc7577
Tobias <tobiasaguilartarea@gmail.com>
2025-07-03 16:41:08 -0300

    Accomplished exercise 4 and 5

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<float> numbers = new List<float>();
+        string userImput = "";
+        int sum = 0;
+        int largestNumber = 0;
+        int smallestPositiveNumber = 100000000;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (userImput != "0")
+        {
+            Console.Write("Enter Number: ");
+            userImput = Console.ReadLine();
+            float givenNumber = float.Parse(userImput);
+            if (userImput != "0")
+                numbers.Add(givenNumber);
+        }
+        ;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > largestNumber)
+            {
+                largestNumber = number;
+            }
+            if (number > 0 && number < smallestPositiveNumber)
+            {
+                smallestPositiveNumber = number;
+            }
+        }
+        List<float> sortedNumbers = numbers;
+        sortedNumbers.Sort();
+        double average = numbers.Average();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+        Console.WriteLine("Sorted list:");
+        foreach (float number in sortedNumbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(favoriteNumber);
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please, enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please, enter your favorite number: ");
+        string number = Console.ReadLine();
+        int favoriteNumber = int.Parse(number);
+        return favoriteNumber;
+    }
+
+    static int SquareNumber(int favoriteNumber)
+    {
+        int squaredNumber = favoriteNumber * favoriteNumber;
+        return squaredNumber;
+    }
+
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
     }
 }

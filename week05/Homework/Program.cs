a2412625ebb040b10a6c4e857cd783cbe5ef6acb
Tobias <tobiasaguilartarea@gmail.com>
2025-08-11 22:57:45 -0300

    Activity 1 done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment1 = new MathAssignment("Samuel Bennet", "Multiplication", "Section 7.3", "Problems 8-19");
+        string summary = mathAssignment1.GetSummary();
+        string homeworkList = mathAssignment1.GetHomeworkList();
+        Console.WriteLine(summary);
+        Console.WriteLine(homeworkList);
+
+        WritingAssingment writingAssingment1 = new WritingAssingment("Mary Waters", "European History", "The Causes of World War II");
+        string writingSummary = writingAssingment1.GetSummary();
+        string writingInfo = writingAssingment1.GetWritingInformation();
+        Console.WriteLine(writingSummary);
+        Console.WriteLine(writingInfo);
+
     }
 }
